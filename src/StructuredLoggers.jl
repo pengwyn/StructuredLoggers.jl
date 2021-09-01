96ffa3f1ed3d01ccd845194225bc012c53edcf4e
Daniel Cocks <daniel.cocks@gmail.com>
2021-09-01 10:00:48 +1000

    Added support for arbitrary target IOs and turning colours off

--- src/StructuredLoggers.jl
+++ src/StructuredLoggers.jl
@@ -1,36 +1,42 @@
 module StructuredLoggers
 
 using Logging, Dates, Match
-using Crayons.Box
+import Crayons
 
 export StructuredLogger,
     global_logger
 
 struct StructuredLogger <: AbstractLogger
     app_name::String
+    io::IO
+    coloured::Bool
+    StructuredLogger(app_name="", io=stderr, coloured=(io isa Base.TTY)) = new(app_name, io, coloured)
 end
-StructuredLogger() = StructuredLogger("")
 
 function Logging.handle_message(logger::StructuredLogger, level, message, _module, group, id, file, line; kwds...)
+    # Dodgy hack for colours
+    C(name) = logger.coloured ? getproperty(Crayons.Box, name) : identity
+    
     # Note: because of the colors, rpad gets confused. So need to pad before the colour is applied.
     level_len = 7
     level_str = @match level begin
-        Logging.Info => GREEN_FG(rpad("info", level_len))
-        Logging.Debug => GREEN_FG(rpad("debug", level_len))
-        Logging.Warn => LIGHT_YELLOW_FG(rpad("warning", level_len))
-        Logging.Error => LIGHT_RED_FG(rpad("error", level_len))
-        _ => WHITE_FG(rpad(string(level), level_len))
+        Logging.Info => C(:GREEN_FG)(rpad("info", level_len))
+        Logging.Debug => C(:GREEN_FG)(rpad("debug", level_len))
+        Logging.Warn => C(:LIGHT_YELLOW_FG)(rpad("warning", level_len))
+        Logging.Error => C(:LIGHT_RED_FG)(rpad("error", level_len))
+        _ => C(:WHITE_FG)(rpad(string(level), level_len))
     end
 
     if logger.app_name != ""
-        message = string(LIGHT_GRAY_FG("$(logger.app_name): ")) * message
+        message = string(C(:LIGHT_GRAY_FG)("$(logger.app_name): ")) * message
     end
-    println(stderr, join([LIGHT_GRAY_FG(Dates.format(now(), "yyyy-mm-ddTHH:MM:SS:sss")),
-                        "[$level_str]",
-                        rpad(message, 30),
-                        (map(collect(kwds)) do (key,val)
-                         string(LIGHT_CYAN_FG(string(key))) * "=" * string(MAGENTA_FG(string(val)))
-                         end)...], " "))
+    println(logger.io, join([C(:LIGHT_GRAY_FG)(Dates.format(now(), "yyyy-mm-ddTHH:MM:SS:sss")),
+                             "[$level_str]",
+                             rpad(message, 30),
+                             (map(collect(kwds)) do (key,val)
+                              string(C(:LIGHT_CYAN_FG)(string(key))) * "=" * string(C(:MAGENTA_FG)(string(val)))
+                              end)...], " "))
+    flush(logger.io)
     nothing
 end
 
